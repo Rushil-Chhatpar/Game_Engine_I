467747b6433fb39bafc08835030da3f1d68542f2
Rushil Chhatpar <chhatpar.rushil@gmail.com>
2024-12-04 19:59:43 -0500

    Updated shader compilation script to throw errors

--- Vulkan_2/shaders/compile.bat
+++ Vulkan_2/shaders/compile.bat
@@ -1,4 +1,16 @@
-D:\Libraries\VulkanSDK\1.3.296.0\Bin\glslc.exe shader.vert -o vert.spv
-D:\Libraries\VulkanSDK\1.3.296.0\Bin\glslc.exe shader.frag -o frag.spv
-
 @echo off
+
+D:\Libraries\VulkanSDK\1.3.296.0\Bin\glslc.exe shader.vert -o vert.spv
+if %errorlevel% neq 0 (
+	echo Failed to compile shader.vert
+	exit /b 1
+	)
+
+D:\Libraries\VulkanSDK\1.3.296.0\Bin\glslc.exe shader.frag -o frag.spv
+if %errorlevel% neq 0 (
+	echo Failed to compile shader.vert
+	exit /b 1
+	)
+	
+echo All shaders compiled successfully.
+exit /b 0
