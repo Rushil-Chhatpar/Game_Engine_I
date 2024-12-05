ff6120e717ae1aff0b89b78f076b741df112472a
Rushil Chhatpar <chhatpar.rushil@gmail.com>
2024-12-04 22:16:05 -0500

    Created index buffer

--- Vulkan_2/Vulkan_2/Mesh.h
+++ Vulkan_2/Vulkan_2/Mesh.h
@@ -9,12 +9,15 @@ public:
 	Mesh()
 	{}
 	// move constructor
-	Mesh(std::vector<Vertex>&& vertices)
+	Mesh(std::vector<Vertex>&& vertices, std::vector<uint32_t>&& indices)
 		: _vertices(std::move(vertices))
+		, _indices(std::move(indices))
 	{}
 
 	const std::vector<Vertex>& GetVertices() const { return _vertices; }
+	const std::vector<uint32_t>& GetIndices() const { return _indices; }
 
 private:
 	std::vector<Vertex> _vertices;
+	std::vector<uint32_t> _indices;
 };
